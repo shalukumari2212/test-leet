41241704a59d27e41e6b4e7ebacd223fa1077fb1
Shalu kumari <shalukumari847401@gmail.com>
2025-09-22 20:02:19 +0530

    Time: 18 ms (14.61%) | Memory: 44.6 MB (43.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,29 +1,47 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        // if(s.length() != t.length()) return false;
 
-        HashMap<Character,Integer>mp= new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch= s.charAt(i);
+        // HashMap<Character,Integer>mp= new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch= s.charAt(i);
+        //     if(!mp.containsKey(ch)){
+        //         mp.put(ch,1);
+        //     }
+        //     else{
+        //         int currfreq= mp.get(ch);
+        //         mp.put(ch,currfreq+1);
+        //     }
+        // }
+        // // decrrease the frequency
+        // for(int i=0;i<t.length();i++){
+        //     char ch= t.charAt(i);
+        //     if(!mp.containsKey(ch)) return false;
+        //     int curr= mp.get(ch);
+        //     mp.put(ch, curr-1);
+        // }
+        // for(var i: mp.values()){
+        //     if(i !=0) return false;
+        // }
+        // return true;
+        if(s.length() != t.length()) return false;
+        HashMap<Character,Integer>mp1= myHashMap(s);
+        HashMap<Character,Integer>mp2= myHashMap(t);
+        return mp1.equals(mp2);
+    }
+     static HashMap<Character,Integer> myHashMap(String str){
+        HashMap<Character,Integer> mp= new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            char ch= str.charAt(i);
             if(!mp.containsKey(ch)){
                 mp.put(ch,1);
             }
             else{
-                int currfreq= mp.get(ch);
-                mp.put(ch,currfreq+1);
+                int currFreq= mp.get(ch);
+                mp.put(ch, currFreq+1);
             }
         }
-        // decrrease the frequency
-        for(int i=0;i<t.length();i++){
-            char ch= t.charAt(i);
-            if(!mp.containsKey(ch)) return false;
-            int curr= mp.get(ch);
-            mp.put(ch, curr-1);
-        }
-        for(var i: mp.values()){
-            if(i !=0) return false;
-        }
-        return true;
+        return mp;
     }
 };
     
