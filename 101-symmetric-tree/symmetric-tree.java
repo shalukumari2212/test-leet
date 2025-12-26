dc18166a5bba3ec9b1869f9a2a2fa721b8ee4904
Shalu kumari <shalukumari847401@gmail.com>
2025-12-26 12:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (42.30%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public boolean isSameTree( TreeNode p,TreeNode q){
+        if(p==null && q==null ) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+    }
+    public TreeNode Inverted(TreeNode root){
+        if(root==null) return null;
+        TreeNode temp= root.left;
+        root.left= root.right;
+        root.right= temp;
+
+        Inverted(root.left);
+        Inverted(root.right);
+        return root;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        // if(root==null)return true;
+        // Inverted(root.left);
+        return(isSameTree(Inverted(root.left),root.right)) ;
+        
+    }
+}
