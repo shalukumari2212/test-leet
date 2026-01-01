50a8d718b7d24124c16e2a515bb44c2e20ad8293
Shalu kumari <shalukumari847401@gmail.com>
2026-01-01 21:22:41 +0530

    Time: 2 ms (19.42%) | Memory: 46.6 MB (46.53%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,28 +1,52 @@
 
 class Solution {
-    TreeNode prev= null;
-    TreeNode first= null;
-    TreeNode second= null;
-
-    public void inorder(TreeNode root){
-        if(root==null) return;
-        inorder(root.left);
-        if(prev!=null && prev.val>root.val){
-            if(first==null) first= prev;
-            second= root;
-        }
-        prev=root;
-        inorder(root.right);
-    }
     public void recoverTree(TreeNode root) {
-        prev= null;
-        first= null;
-        second= null;
-        inorder(root);
-        int temp= first.val;
-        first.val = second.val;
-        second.val= temp;
-
+        TreeNode curr= root;
+        TreeNode prev= null;
+        TreeNode prevprev=null;
+        List<TreeNode>l= new ArrayList<>();
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred= curr.left;
+                while(pred.right!=null && pred.right!=curr) pred=pred.right;
+                if(pred.right==null){
+                    pred.right=curr;
+                    curr=curr.left;
+                }
+                if(pred.right==curr){
+                    pred.right=null;
+                    //visit
+                    if(prev!=null && prevprev!=null){
+                        if(prev.val<curr.val && prev.val<prevprev.val) l.add(prev);
+                        if(prev.val>curr.val && prev.val>prevprev.val) l.add(prev);
+                    }
+                    else if(prev!=null && prev.val>curr.val) l.add(prev);
+                    
+                    prevprev=prev;
+                    prev=curr;
+                    curr=curr.right;
+                }
+            }
+        
+        else{
+                //visit
+            if(prev!=null && prevprev!=null){
+                if(prev.val<curr.val && prev.val<prevprev.val) l.add(prev);
+                if(prev.val>curr.val && prev.val>prevprev.val) l.add(prev);
+            } 
+            else if(prev!=null && prev.val>curr.val) l.add(prev); 
+            prevprev=prev;
+            prev=curr;
+            curr=curr.right;
+        }
+        }
+    if(prev.val<prevprev.val) l.add(prev);
+    TreeNode first= l.get(0);
+    TreeNode second= l.get(l.size()-1);
+    int temp= first.val;
+    first.val= second.val;
+    second.val=temp;
 
     }
+    
 }
