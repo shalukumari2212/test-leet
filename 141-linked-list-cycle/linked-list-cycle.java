02d62ad6ef70a904d57ad8d4ea72456628bed005
Shalu kumari <shalukumari847401@gmail.com>
2026-01-03 22:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (29.70%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        if(head==null) return false;
+        if(head.next==null) return false;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
