d52dc4fee5d1c93b099317630aa1ccafc9c62868
Shalu kumari <shalukumari847401@gmail.com>
2026-03-15 07:29:33 +0530

    Time: 13 ms (28.89%) | Memory: 52.6 MB (85.10%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -4,14 +4,23 @@ class Solution {
         for(int num :nums){
             map.put(num , map.getOrDefault(num,0)+1);
         }
-        int max=0;
-        int ans=0;
+        // int max=0;
+        // int ans=0;
+        // for(int key : map.keySet()){
+        //     if(map.get(key)>max){
+        //         max=map.get(key);
+        //         ans= key;
+        //     }
+        // }
+        // return ans;
+
+        int n= nums.length;
+       
         for(int key : map.keySet()){
-            if(map.get(key)>max){
-                max=map.get(key);
-                ans= key;
+            if(map.get(key)>n/2){
+                return key;
             }
         }
-        return ans;
+        return -1;
     }
 }
