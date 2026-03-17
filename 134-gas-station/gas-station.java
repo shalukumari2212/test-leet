a2cb4d61b0a5298e4e2b2b8ccc316d3e61a51668
Shalu kumari <shalukumari847401@gmail.com>
2026-03-17 12:21:29 +0530

    Time: 2 ms (99.25%) | Memory: 119.3 MB (78.65%) - LeetSync

---
+++ 134-gas-station/gas-station.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totgas=0 , totcost=0;
+
+        // find total  gas 
+        for(int val : gas){
+            totgas += val;
+        }
+
+        //find total cost
+
+        for(int val: cost){
+            totcost += val;
+        }
+        
+        //find exist or not
+        if(totgas < totcost) return -1;
+
+        //find 
+        int start=0 , curGas=0;
+        for(int i=0 ;i<gas.length ;i++){
+            curGas += gas[i] - cost [i];
+
+            if(curGas < 0) {
+                start=i+1;
+                curGas =0;
+            }
+
+        }
+        return start;
+    }
+}
