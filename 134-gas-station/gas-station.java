d72da56b1a49da0cc90e389ea0fc99fa503d1ea7
Shalu kumari <shalukumari847401@gmail.com>
2026-03-17 12:25:09 +0530

    Time: 2 ms (99.25%) | Memory: 119.6 MB (20.04%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -2,31 +2,45 @@ class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int totgas=0 , totcost=0;
 
-        // find total  gas 
-        for(int val : gas){
-            totgas += val;
-        }
+        // // find total  gas 
+        // for(int val : gas){
+        //     totgas += val;
+        // }
 
-        //find total cost
+        // //find total cost
 
-        for(int val: cost){
-            totcost += val;
-        }
+        // for(int val: cost){
+        //     totcost += val;
+        // }
         
-        //find exist or not
-        if(totgas < totcost) return -1;
+        // //find exist or not
+        // if(totgas < totcost) return -1;
 
-        //find 
+        // //find 
+        // int start=0 , curGas=0;
+        // for(int i=0 ;i<gas.length ;i++){
+        //     curGas += gas[i] - cost [i];
+
+        //     if(curGas < 0) {
+        //         start=i+1;
+        //         curGas =0;
+        //     }
+
+        // }
+        // return start;
         int start=0 , curGas=0;
-        for(int i=0 ;i<gas.length ;i++){
-            curGas += gas[i] - cost [i];
+        for(int i=0;i< gas.length ;i++){
+            totgas+= gas[i];
+            totcost += cost[i];
 
+            curGas += gas[i] -cost[i];
             if(curGas < 0) {
                 start=i+1;
                 curGas =0;
-            }
+            } 
+            
 
         }
-        return start;
+        return totgas < totcost ? -1 :start;
     }
 }
