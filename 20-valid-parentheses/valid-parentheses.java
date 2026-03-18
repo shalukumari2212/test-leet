890638e8a16e886d09aea4aaaa35555b30cd63e2
Shalu kumari <shalukumari847401@gmail.com>
2026-03-18 21:20:16 +0530

    Time: 3 ms (87.46%) | Memory: 43.4 MB (53.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+
+            if(ch== '(' || ch== '{' || ch== '['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                if (( ch == ')' && st.pop()!= '(') ||
+                   ( ch == '}' &&  st.pop()!= '{') ||
+                   ( ch == ']' &&  st.pop()!= '['))
+                   return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
