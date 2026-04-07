f93d090015a41b7ca55613e4a01d1f7d41b1b109
Shalu kumari <shalukumari847401@gmail.com>
2026-04-07 21:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (63.67%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -6,12 +6,25 @@ class Solution {
         int skip = rob2(nums,i+1,dp);
         return dp[i]= Math.max(take,skip);
     }
+    public int maxAmount(int []nums, int[]dp,int n){
+        if (n == 1) return nums[0];
+        dp[0]= nums[0];
+        dp[1] = Math.max(nums[0] , nums[1]);
+        // int n= nums.length;
+
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max( nums[i]+ dp[i-2] , dp[i-1]);
+
+        }
+        return dp[n-1];
+    }
 
     public int rob(int[] nums) {
         int n= nums.length;
         int [] dp = new int[n];
-        Arrays.fill(dp,-1);
+        return maxAmount(nums,dp,n);
+        // Arrays.fill(dp,-1);
     
-     return rob2(nums,0,dp);
+    //  return rob2(nums,0,dp);
     }
 }
