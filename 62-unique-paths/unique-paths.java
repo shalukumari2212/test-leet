8e636febc6a684210b7dee0eb3cf8ddfca1858f5
Shalu kumari <shalukumari847401@gmail.com>
2026-04-09 19:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (85.07%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,31 @@
 class Solution {
-    public int path(int i,int j,int m ,int n,int[][] dp){
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 || j==n-1) return 1;
-        if(dp[i][j]!= -1) return dp[i][j];
-        int rightways = path(i,j+1,m,n,dp);
-        int downtways = path(i+1,j,m,n,dp);
-        return dp[i][j]=rightways + downtways;
-    }
+    // public int path(int i,int j,int m ,int n,int[][] dp){
+    //     if(i>=m || j>=n) return 0;
+    //     if(i==m-1 || j==n-1) return 1;
+    //     if(dp[i][j]!= -1) return dp[i][j];
+    //     int rightways = path(i,j+1,m,n,dp);
+    //     int downtways = path(i+1,j,m,n,dp);
+    //     return dp[i][j]=rightways + downtways;
+    // }
+    // int[][] dp = new int[m][n];
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             dp[i][j] =-1;
+    //         }
+    //     }   
+    //     return path(0,0,m,n,dp);
+
     public int uniquePaths(int m, int n) {   
-        int[][] dp = new int[m][n];
+        int[][] dp= new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j] =-1;
+                if(i==0 || j==0) dp[i][j]=1;
+                else{
+                    dp[i][j]= dp[i-1][j]+dp[i][j-1];
+                }
             }
-        }   
-        return path(0,0,m,n,dp);
+        }
+        return dp[m-1][n-1];
+
     }
 }
