1ec86395ed4e0e129f4a53b52b0ba6d3f260bcbd
Shalu kumari <shalukumari847401@gmail.com>
2026-04-10 22:29:28 +0530

    Time: 26 ms (78.50%) | Memory: 44.4 MB (78.40%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -16,10 +16,25 @@ class Solution {
         
     }
     public int numSquares(int n) {
-      
-       int[]dp= new int[n+1];
-       Arrays.fill(dp,-1);
-        return squares(n,dp);
+      int[] dp = new int [n+1];
+       
+      for(int i=1;i<=n;i++){
+        if(isPerfectSqr(i)) dp[i] = 1;
+        else{
+            int min= Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+              int count= dp[j*j]+dp[i-j*j];
+              min=Math.min(min,count);
+            }
+            dp[i]=min;
+        }
+        
+      }
+      return dp[n];
+
+    //    int[]dp= new int[n+1];
+    //    Arrays.fill(dp,-1);
+    //     return squares(n,dp);
 
     }
 }
