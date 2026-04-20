cfa48318b3d8028cf5553dd288fa5b7a0f0bb63c
Shalu kumari <shalukumari847401@gmail.com>
2026-04-20 23:57:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (22.39%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,5 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        // int left = 0;
+        // int right= nums.length-1;
+        // while(left<=right){
+        //     int mid= (left+right)/2;
+
+        //     if((nums[mid] >= nums[mid-1] ) && (nums[mid]>nums[mid+1])) return mid;
+        //     if(nums[mid] > nums[mid-1]) left=mid+1;
+        //     else right= mid+1;
+        // }
+        // return -1;
+
         for(int i=0;i<nums.length;i++){
             boolean left= (i==0) || nums[i]>nums[i-1] ;
             boolean right=  (i== nums.length-1 || nums[i]>nums[i+1]);
@@ -7,5 +18,5 @@ class Solution {
            
         }
          return -1;
-    }
-}
+    
+}}
