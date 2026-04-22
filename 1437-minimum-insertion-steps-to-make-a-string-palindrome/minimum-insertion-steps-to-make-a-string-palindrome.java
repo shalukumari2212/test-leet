624f37d2aabe3dd0b8374c32a51ad454a04c75c5
Shalu kumari <shalukumari847401@gmail.com>
2026-04-22 15:20:52 +0530

    Time: 48 ms (5.43%) | Memory: 47.3 MB (60.58%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,24 +1,33 @@
 class Solution {
-    public int lps( int  i, int j,StringBuilder s,int[][]dp ){
-     if(i>j) return 0;
-     if(i==j) return 1;
-     if(dp[i][j]!= -1) return dp[i][j];
-     if(s.charAt(i) == s.charAt(j)) return dp[i][j]=  2 + lps(i+1 ,j-1 ,s,dp);
-     else return dp[i][j]=Math.max(lps(i+1,j,s,dp) , lps(i,j-1,s,dp));
+   
+    public int lcs(int i,int j,StringBuilder a,StringBuilder b ,int[][]dp) {
+        if(i==-1 || j==-1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(a.charAt(i) == b.charAt(j)) return dp[i][j]=1+ lcs(i-1,j-1,a,b,dp);
+        else return dp[i][j]= Math.max(lcs(i-1,j,a,b,dp),lcs(i,j-1,a,b,dp));
     }
-    public int longestPalindromeSubseq(String str) {
-       StringBuilder s= new StringBuilder(str);
-        int n= s.length();
-
-        int[][]dp= new int[n][n];
-        for(int i = n-1; i >= 0; i--){
-          for(int j = i; j < n; j++) dp[i][j] =-1;          
-        }
-        return lps(0,n-1,s,dp);
+    public int lcs2(String str1,String str2) {
+      int n=str1.length();
+      int m=str2.length();
+      StringBuilder a= new StringBuilder(str1);
+      StringBuilder b= new StringBuilder(str2);
+      int[][]dp = new int[n][m];
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) dp[i][j]=-1;
+      }
+      return lcs(n-1,m-1,a,b,dp);
     }
-
+    public String reverse(String s) {
+        StringBuilder a= new StringBuilder(s);
+        a.reverse();
+        return a.toString(); 
+    }
+    public int longestPalindromeSubseq(String s) {
+        return lcs2(s, reverse(s));
+    }
+    
     public int minInsertions(String s) {
         int n= s.length();
-        return n - (longestPalindromeSubseq(s));
+        return n- longestPalindromeSubseq(s);
     }
 }
