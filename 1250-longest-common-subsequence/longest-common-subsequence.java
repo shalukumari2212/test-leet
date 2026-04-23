95fff7a9d3c0f12a43b26f13cadcad9f71af9e8b
Shalu kumari <shalukumari847401@gmail.com>
2026-04-23 21:49:37 +0530

    Time: 28 ms (18.57%) | Memory: 54.1 MB (36.78%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,47 +3,37 @@ class Solution {
         if(i==-1 || j==-1) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         if(a.charAt(i)== b.charAt(j)) return dp[i][j]= 1+lcs(i-1,j-1,a,b,dp);
-        else{
-            return dp[i][j]= Math.max(lcs(i-1,j,a,b,dp) , lcs(i,j-1,a,b,dp));
-        }
+        else  return dp[i][j]= Math.max(lcs(i-1,j,a,b,dp) , lcs(i,j-1,a,b,dp));
 
     }
     
     public int longestCommonSubsequence(String text1, String text2) {
-       int n= text1.length();
-       int m= text2.length();
-       StringBuilder a = new StringBuilder(text1);
-       StringBuilder b = new StringBuilder(text2);
-       int [][]dp= new int[n][m];
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++) dp[i][j]=-1;
-       }
-       return lcs(n-1,m-1,a,b,dp);
-    }
-}
-
-
-// public int lcs(int i ,int j, StringBuilder a, StringBuilder b ,int[][]dp){     
-//         if(i<0 || j<0) return 0;
-//         if(dp[i][j]!= -1) return dp[i][j];
        
-//         if(a.charAt(i) == b.charAt(j)) return dp[i][j]= 1+lcs(i-1,j-1,a,b,dp);
-//         else return dp[i][j]=Math.max(lcs(i,j-1,a,b,dp) ,lcs(i-1,j,a,b,dp));
+        int n= text1.length();
+        int m= text2.length();
+        int [][]dp= new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++) {
+                int p = (i>0 && j>0) ? dp[i-1][j-1] :0;
+                int q= (i>0) ?  dp[i-1][j] :0;
+                int r= (j>0) ? dp[i][j-1]:0;
+            
+                if(text1.charAt(i)== text2.charAt(j)) dp[i][j]= 1+p;
+                else dp[i][j]= Math.max( q,r )  ;
+            }
+        }
+        return dp[n-1][m-1];
+    
+    }
+   
+}
+ // int n= text1.length();
+    //    int m= text2.length();
+    //    StringBuilder a = new StringBuilder(text1);
+    //    StringBuilder b = new StringBuilder(text2);
+    //    int [][]dp= new int[n][m];
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++) dp[i][j]=-1;
+    //    }
+    //    return lcs(n-1,m-1,a,b,dp);
 
-
-//     }
-
-
-//  StringBuilder a = new StringBuilder(text1);
-//         StringBuilder b = new StringBuilder(text2);
-
-//         int m=a.length() , n= b.length();
-
-//         int[][]dp= new int[m][n];
-//         for(int i=0;i<m ;i++){
-//             for(int j=0;j<n;j++){
-//                 dp[i][j]=-1;
-//             }
-//         }
-
-//         return lcs(m-1,n-1,a,b,dp);
