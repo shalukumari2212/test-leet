3eb2a1d0d73d5cec60aeaa9b77e2c51303fa2a38
Shalu kumari <shalukumari847401@gmail.com>
2026-04-25 15:33:46 +0530

    Time: 17 ms (30.88%) | Memory: 53.2 MB (5.14%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,27 +1,61 @@
 class Solution {
-    public boolean isPalindrome(String s){
-        int i=0;
-        int j= s.length()-1;
-        while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            }
-            else return false;
-        }
-        return true;
-    }
+    //METHOD 1
+    // public boolean isPalindrome(String s){
+    //     int i=0;
+    //     int j= s.length()-1;
+    //     while(i<j){
+    //         if(s.charAt(i) == s.charAt(j)){
+    //             i++;
+    //             j--;
+    //         }
+    //         else return false;
+    //     }
+    //     return true;
+    // }
     public int countSubstrings(String s) {
         int n= s.length();
         int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1; j<=n;j++){
-                if(isPalindrome(s.substring(i,j))){
+        int[][]dp= new int [n][n];
+        for(int k=0;k<n;k++){
+            int i=0; int j=k;
+            while(j<n){
+                if(i==j) {
+                    dp[i][j]=1;
                     count++;
                 }
+                else if(s.charAt(i) == s.charAt(j)){
+                    if(j==i+1) {dp[i][j]=1; count++;}
+                    else {
+                        dp[i][j]= dp[i+1][j-1];
+                        if(dp[i][j]==1) count++;
+                        
+                        }
+                }
+                else dp[i][j]=0;
+                i++; j++;
             }
         }
         return count;
-
     }
 }
+    //Method2
+// int n= s.length();
+//         int count=0;
+//         int[][]dp = new int[n][n];
+
+//         for(int i=0;i<n;i++){            
+//             if(i==i) dp[i][j]=1;  // diagonal fill         
+//         }
+
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=i+1; j<n;j++){
+//                 if(s.charAt(i) == s.charAt(j)) {
+//                     if(j)
+
+//                 }
+//                 else dp[i][j]= dp[i+1][j-1];
+//                 count+= dp[i][j];
+//             }
+//         }
+
+//         return count;
