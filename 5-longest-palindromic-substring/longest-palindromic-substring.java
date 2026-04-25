088c6d83881ddc55f267df20cb713619d452f8b5
Shalu kumari <shalukumari847401@gmail.com>
2026-04-25 16:01:07 +0530

    Time: 152 ms (21.44%) | Memory: 105.8 MB (5.08%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -11,40 +11,42 @@ class Solution {
     //     }
     //     return true;
     // }
+
     public String longestPalindrome(String s) {
         int n= s.length();
-
+        int count=0;
         int start=0;
         int maxlen=1;
-        int[][]dp = new int[n][n];
-
-        for(int i=0;i<n;i++){            
-            if(i==i) dp[i][i]=1;
-              // diagonal fill         
-        }
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=i+1; j<n;j++){
-                if(s.charAt(i) == s.charAt(j)) {
+        int[][]dp= new int [n][n];
+        for(int k=0;k<n;k++){
+            int i=0; int j=k;
+            while(j<n){
+                if(i==j) {
+                    dp[i][j]=1;
+                    
+                }
+                else if(s.charAt(i) == s.charAt(j)){
                     if(j==i+1) {
                         dp[i][j]=1; 
+                        
                     }
-                    else dp[i][j]= dp[i+1][j-1];
-                    
-
+                    else {
+                        dp[i][j]= dp[i+1][j-1];
+                        
+                    }
                 }
                 else dp[i][j]=0;
+                
 
-                // fill
-                    
-                    if(dp[i][j] ==1 && (j-i+1 )> maxlen){
-                        start=i;
-                        maxlen= j-i+1;
-                    } 
-                             
+                if(dp[i][j] ==1 && (j-i+1) > maxlen){
+                    start=i;
+                    maxlen= j-i+1;
+                }
+                i++; j++;
             }
+         
         }
-        return s.substring(start,start+maxlen);
+return s.substring(start,start+maxlen);
     }
 }
 
@@ -67,3 +69,32 @@ class Solution {
 //             }
 //         }
 //         return str;
+
+
+// METHOD2
+// int n= s.length();
+
+//         int start=0;
+//         int maxlen=1;
+//         int[][]dp = new int[n][n];
+//         for(int i=0;i<n;i++){            
+//             if(i==i) dp[i][i]=1; // diagonal fill                       
+//         }
+
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=i+1; j<n;j++){
+//                 if(s.charAt(i) == s.charAt(j)) {
+//                     if(j==i+1) dp[i][j]=1;                     
+//                     else dp[i][j]= dp[i+1][j-1];
+//                 }
+//                 else dp[i][j]=0;
+
+//                 // fill                    
+//                 if(dp[i][j] ==1 && (j-i+1 )> maxlen){
+//                     start=i;
+//                     maxlen= j-i+1;
+//                 } 
+//         return s.substring(start,start+maxlen);
+                             
+//             }
+//         }
