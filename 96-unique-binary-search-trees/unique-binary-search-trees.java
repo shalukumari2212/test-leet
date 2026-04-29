15e7b9be3c26a2fbda312d7fe58d7cb8b1b17eb5
Shalu kumari <shalukumari847401@gmail.com>
2026-04-29 19:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (50.66%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -4,9 +4,14 @@ class Solution {
         if(n==2) return 2;
         int []dp = new int[n+1];
         dp[0]=1; dp[1]=1; dp[2]=2;
+        // for(int i=3;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         dp[i]+= dp[j-1] * dp[i-j];
+        //     }
+        // }
         for(int i=3;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                dp[i]+= dp[j-1] * dp[i-j];
+            for(int j=0;j<i;j++){
+                dp[i]+= dp[j] * dp[i-j-1];
             }
         }
         return dp[n];
