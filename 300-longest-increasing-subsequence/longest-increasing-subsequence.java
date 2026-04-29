044977d06ac0aa5660eb2d368e6786b574b8fe95
Shalu kumari <shalukumari847401@gmail.com>
2026-04-29 14:13:18 +0530

    Time: 40 ms (51.90%) | Memory: 46.2 MB (40.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,5 @@
 class Solution {
-    // public int LIC(int i,int[] nums,int[]dp){
-    //     if(i==nums.length){
-            
-    //       return 0;
-    //     }
-    //     if(dp[i]!=-1) return dp[i]; 
-    //     int skip  = LIC(i+1,nums,dp);
-        
-    //     if(dp[i]==-1 || nums[i-1]<nums[i]){
-    //         int take= 1 +LIC(i+1,nums ,dp);
-    //         dp[i]= Math.max(take,skip);
-    //     }
-    //     return dp[i];
-        
-       
 
-    // }
     public int lengthOfLIS(int[] nums) {
         int n= nums.length;
         int maxlen=1;
@@ -30,10 +14,9 @@ class Solution {
               
             }
            maxlen= Math.max(maxlen,dp[i]);
+          
         }
         return maxlen;
-        // int[]dp =new int[nums.length];
-        // Arrays.fill(dp,-1);
-        // return  LIC(0 ,nums,dp );
+       
     }
 }
