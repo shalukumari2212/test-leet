33cb570b593f5e319b1ebf37a3bf1197b6c98b4b
Shalu kumari <shalukumari847401@gmail.com>
2026-05-04 17:42:06 +0530

    Time: 1 ms (86.02%) | Memory: 46 MB (90.92%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,23 +1,37 @@
 class Solution {
-    public void bfs(int i, List<List<Integer>> adj ,boolean[] isVisited ) {
-        Queue<Integer> q= new LinkedList<>();
-        q.add(i);
-        while(q.size()>0){
-            int front= q.remove();
-            for(int ele: adj.get(front)){
-                if(!isVisited[ele]){
-                    isVisited[ele]= true;
-                    q.add(ele);
-                }
-            }
+    // public void bfs(int i, List<List<Integer>> adj ,boolean[] isVisited ) {
+    //     Queue<Integer> q= new LinkedList<>();
+    //     q.add(i);
+    //     while(q.size()>0){
+    //         int front= q.remove();
+    //         for(int j=0;j<front.size();j++){
+    //             if(!isVisited[front[i]]){
+    //                 q.add(front[i]);
+    //                 isVisited[front[i]] =true;
+    //             }
+    //         }
+    //         // for(int ele: adj.get(front)){
+    //         //     if(!isVisited[ele]){
+    //         //         isVisited[ele]= true;
+    //         //         q.add(ele);
+    //         //     }
+    //         // }
+    //     }
+    //     return;
+    // }
+    public void dfs(int i, List<List<Integer>> adj ,boolean[] isVisited ) {
+        isVisited[i]= true;
+
+        for(int ele: adj.get(i)){
+            if(!isVisited[ele]) dfs(ele,adj,isVisited);
         }
-        return;
     }
     public boolean canVisitAllRooms(List<List<Integer>> adj) {
         int n= adj.size();
         boolean [] isVisited= new boolean[n];
         isVisited[0] = true;
-        bfs(0,adj,isVisited);
+        // bfs(0,adj,isVisited);
+        dfs(0,adj,isVisited);
         for(boolean ele : isVisited){
             if(ele== false) return false;
         }
