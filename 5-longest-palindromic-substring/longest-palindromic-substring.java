7581fac19494c447e1a933d3c2008c643ad96d68
Shalu kumari <shalukumari847401@gmail.com>
2026-05-26 22:01:39 +0530

    Time: 145 ms (22.36%) | Memory: 105.8 MB (5.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -18,35 +18,28 @@ class Solution {
         int start=0;
         int maxlen=1;
         int[][]dp= new int [n][n];
+
         for(int k=0;k<n;k++){
             int i=0; int j=k;
             while(j<n){
-                if(i==j) {
-                    dp[i][j]=1;
-                    
-                }
+                if(i==j) dp[i][j]=1;
                 else if(s.charAt(i) == s.charAt(j)){
-                    if(j==i+1) {
-                        dp[i][j]=1; 
-                        
-                    }
-                    else {
-                        dp[i][j]= dp[i+1][j-1];
-                        
-                    }
+                    if(j==i+1) dp[i][j]=1;                    
+                    else dp[i][j]= dp[i+1][j-1];         
                 }
                 else dp[i][j]=0;
                 
-
+                // fill the string
                 if(dp[i][j] ==1 && (j-i+1) > maxlen){
                     start=i;
                     maxlen= j-i+1;
                 }
+                
                 i++; j++;
             }
          
         }
-return s.substring(start,start+maxlen);
+        return s.substring(start,start+maxlen);
     }
 }
 
@@ -62,10 +55,8 @@ return s.substring(start,start+maxlen);
 //                 String sub= s.substring(i,j);
 //                 if(isPalindrome(sub) && sub.length()>maxLen ){
 //                     str=sub;
-//                     maxLen= sub.length();
-                    
+//                     maxLen= sub.length(); 
 //                 }
-
 //             }
 //         }
 //         return str;
@@ -94,7 +85,7 @@ return s.substring(start,start+maxlen);
 //                     start=i;
 //                     maxlen= j-i+1;
 //                 } 
-//         return s.substring(start,start+maxlen);
+//              return s.substring(start,start+maxlen);
                              
 //             }
 //         }
