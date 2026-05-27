33bf1345734d8d430084267f1e27446edbb1265d
Shalu kumari <shalukumari847401@gmail.com>
2026-05-27 23:50:01 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (74.47%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+
+        // 1. Ignore leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // 3. Convert digits
+        long num = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            num = num * 10 + digit;
+
+            // 4. Handle overflow
+            if (sign * num > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (sign * num < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int)(sign * num);
+    }
+}
